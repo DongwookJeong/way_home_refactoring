b82f790f4fb8b98c860d87bb83f705126c64b851
dongwookjeong <nolhoon211@gmail.com>
2022-10-25 18:43:27 +0900

    fs, server 두걸음

--- server.js
+++ server.js
@@ -5,10 +5,18 @@ const server = http.createServer()
 server.on('request', doRequest)
 server.listen(7378)
 console.log('server running')
+
 function doRequest(req, res){
-fs.readFile('./hello.html', 'utf-8', function(err, data){
-  res.writeHead(200, {'Content-Type': 'text/html'})
-  res.write(data)
-  res.end()
+  let number = Math.floor(Math.random()*3)
+  fs.readFile('./hello.html', 'utf-8', 
+    function(err, data){
+      let title = ["pageA", "pageB", "pageC"]
+      let content = ["this is sample.", "another content", "last content"]
+      let data2 = data.
+        replace(/@title@/g, title[number]).
+        replace(/@content@/g, content[number])
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    res.write(data2)
+    res.end()
 })
 }
